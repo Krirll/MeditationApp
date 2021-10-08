f27de609db740f8e14215270c83d7ecd6c3bba7d
Krirll <balalaev20022@gmail.com>
2021-10-08 23:59:51 +0300

    доделал адаптер для фото

--- app/src/main/java/com/learn/meditationapp/adapters/PhotoAdapter.kt
+++ app/src/main/java/com/learn/meditationapp/adapters/PhotoAdapter.kt
@@ -1,4 +1,67 @@
 package com.learn.meditationapp.adapters
 
-class PhotoAdapter {
+import android.graphics.drawable.Drawable
+import android.renderscript.ScriptGroup
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.learn.meditationapp.Photo
+import com.learn.meditationapp.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.InputStream
+import java.net.URL
+
+class PhotoAdapter(private val list : List<Photo>) : RecyclerView.Adapter<PhotoAdapter.MyViewHolder>() {
+
+    class MyViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        var time : TextView? = itemView.findViewById(R.id.time)
+        var image : ImageView? = itemView.findViewById(R.id.photo)
+        var add : CardView? = itemView.findViewById(R.id.addButton)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+                                     .inflate(
+                                         if (viewType == R.layout.photo_item)
+                                             R.layout.photo_item
+                                         else R.layout.button_item, parent, false)
+        return MyViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        if (position == list.size) {
+            holder.add?.setOnClickListener {
+
+            }
+        }
+        else {
+            holder.time?.text = list[position].time
+            CoroutineScope(Dispatchers.Main).launch {
+                var image : Drawable
+                withContext(Dispatchers.IO) {
+                    image =
+                        Drawable.createFromStream(
+                            URL(list[position].image).content as InputStream, "src"
+                        )
+                }
+                holder.image?.setImageDrawable(image)
+            }
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (position == list.size) R.layout.button_item else R.layout.photo_item
+    }
+
+    override fun getItemCount(): Int {
+        return list.size + 1
+    }
+
 }
--- app/src/main/java/com/learn/meditationapp/fragments/ProfileFragment.kt
+++ app/src/main/java/com/learn/meditationapp/fragments/ProfileFragment.kt
@@ -1,11 +1,28 @@
 package com.learn.meditationapp.fragments
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Room
+import com.learn.meditationapp.API.RetrofitObject
+import com.learn.meditationapp.DataBase
+import com.learn.meditationapp.Photo
 import com.learn.meditationapp.R
+import com.learn.meditationapp.adapters.PhotoAdapter
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.InputStream
+import java.net.URL
 
 class ProfileFragment : Fragment() {
 
@@ -18,6 +35,29 @@ class ProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //todo
+        CoroutineScope(Dispatchers.Main).launch {
+            val image : Drawable
+            withContext(Dispatchers.IO) {
+                image =
+                    Drawable.createFromStream(
+                        URL(requireArguments().getString("AVATAR")).content as InputStream, "src"
+                    )
+            }
+            view.findViewById<ImageView>(R.id.avatarProfile).setImageDrawable(image)
+        }
+        view.findViewById<TextView>(R.id.nickProfile).text = requireArguments().getString("NAME")
+        val recyclerView = view.findViewById<RecyclerView>(R.id.photos)
+        recyclerView.layoutManager = LinearLayoutManager(view.context)
+        CoroutineScope(Dispatchers.Main).launch {
+            val dataBase : DataBase
+            val list : List<Photo>
+            withContext(Dispatchers.IO) {
+                dataBase =
+                    Room.databaseBuilder(view.context, DataBase::class.java, "photos")
+                        .build()
+                list = dataBase.photoDao().getAll()
+            }
+            recyclerView.adapter = PhotoAdapter(list)
+        }
     }
 }
